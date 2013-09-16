0700fcd4280abfc65dbeb598148edf57e8533c46
Nick Veys <nveys@aramisgroup.com>
2013-09-15 21:23:28 -0500

    Adding some specs for sample data

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -2,8 +2,63 @@ require 'spec_helper'
 
 describe Douglas::NE::Checks::File do
 
-  subject { Douglas::NE::Checks::File.new(data_files.first) }
+  before(:all) do
+    @file = Douglas::NE::Checks::File.new(data_files.first)
+  end
 
-  it { should respond_to(:to_s) }
+  subject { @file }
+
+  context '12532 - COUNTY ROAD' do
+
+    subject { @file.dump['12532 - COUNTY ROAD'] }
+
+    it { should have(3).keys }
+
+    its(:keys) { should include('670011 - DESIGN & SURVEY') }
+    its(:keys) { should include('672011 - MAINTENANCE') }
+    its(:keys) { should include('674011 - EQUIPMENT') }
+
+    context '670011 - DESIGN & SURVEY' do
+
+      subject { @file.dump['12532 - COUNTY ROAD']['670011 - DESIGN & SURVEY'] }
+
+      it { should have(1).row }
+
+      context 'row 0' do
+
+        subject { @file.dump['12532 - COUNTY ROAD']['670011 - DESIGN & SURVEY'][0] }
+
+        it 'should have known values' do
+          expect(subject[:supplier]).to eql('METROPOLITAN AREA PLANNING AGENCY')
+          expect(subject[:account]).to eql('42239 - PROFESSIONAL FEES - OTHER')
+          expect(subject[:description]).to eql('INV 2608 (NIROC FUNDING)')
+          expect(subject[:invoice]).to eql('2603')
+          expect(subject[:check_number]).to eql('363827')
+          expect(subject[:check_date]).to eql('10-SEP-2013')
+          expect(subject[:check_status]).to eql('NEGOTIABLE')
+          expect(subject[:amount]).to be_within(0.01).of(25000.00)
+        end
+
+      end
+
+    end
+
+    context '672011 - MAINTENANCE' do
+
+      subject { @file.dump['12532 - COUNTY ROAD']['672011 - MAINTENANCE'] }
+
+      it { should have(30).rows }
+
+    end
+
+    context '674011 - EQUIPMENT' do
+
+      subject { @file.dump['12532 - COUNTY ROAD']['674011 - EQUIPMENT'] }
+
+      it { should have(2).rows }
+
+    end
+
+  end
 
 end
